47e9426e390eb6899afd6f086dc7a31184d5f079
tshikamisava <lhlongwane81@gmail.com>
2023-11-11 01:07:00 +0000

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SideNav from './components/screens/SideNav';
-
-const Home = () => <div>Home Page</div>;
-const CreateAccount = () => <div>Create Account Content</div>;
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <SideNav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create-account" element={<CreateAccount />} />
-          {/* Add other routes */}
-        </Routes>
-      </div>
-    </Router>
-=======
 import './App.css';
 import Verification from './components/Screens/Verification';
 
@@ -28,7 +7,6 @@ function App() {
       <Verification />	
       
     </div>
->>>>>>> 9faeaa879183a7305dbf876ecfb4b66dafa6622a
   );
 }
 

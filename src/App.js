744ad5452d3246419d8a24e8da9833123b23162d
TemoshoS <temoshomaduane@gmail.com>
2023-11-11 03:05:17 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Verification from './components/Screens/Verification';
+import TransactionHistory from './components/Screens/TransactionHistory';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Verification />	
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Verification />} />
+          <Route path="/transaction-history" element={<TransactionHistory />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

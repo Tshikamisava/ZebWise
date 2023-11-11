bb85fba7f8566431f1f3408a66cd78252335711a
tshikamisava <lhlongwane81@gmail.com>
2023-11-11 17:56:34 +0000

    bookeper

--- src/App.js
+++ src/App.js
@@ -1,39 +1,8 @@
-import './App.css';
-<<<<<<< HEAD
-import BookeeperReport from './components/BookeeperReport';
-// import SalesSummary from './components/BookeeperReport';
-// import SalesReport from './components/BookeeperReport';
-// import GeneratePayment from './components/screens/GeneratePayment';
-// import SignUp from './components/screens/SignUp';
-// import Verification from './components/screens/Verification';
-// import TransactionHistory from './components/screens/TransactionHistory';
-// import Dashboard from './components/screens/Dashboard';
-// // import Home from './components/screens/Home';
-// import BookKeeper from './components/screens/BookKeeper'; 
-//import CreateAccount from './components/screens/CreateAccount'; 
 
-import React from 'react';
-
-function App() {
-  // const [content, setContent] = React.useState('Home Page');
-
-  return (
-    <div className="App">
-     <BookeeperReport />
-        {/* <GeneratePayment />  */}
-      {/* <SignUp /> */}
-      {/* <TransactionHistory /> */}
-      {/* <Verification /> */}
-      {/* <Dashboard /> */}
-      {/* <Home /> */}
-      {/* <BookKeeper /> */}
-      {/* <CreateAccount /> */}
-    </div>
-=======
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SideNav from './components/Screens/SideNav';
-import Verification from './components/Screens/Verification';
+import SideNav from './components/screens/SideNav.jsx';
+import Verification from './components/screens/Verification';
 import TransactionHistory from './components/Screens/TransactionHistory';
 import Dashboard from './components/Screens/Dashboard';
 import Home from './components/Screens/Home';
@@ -64,7 +33,6 @@ function App() {
         </Routes>
       </div>
     </Router>
->>>>>>> 47802773a19ac15443827235481f33d41819747f
   );
 }
 

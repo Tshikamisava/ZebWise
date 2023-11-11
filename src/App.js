d22f9a2e4f2007bd998aa5866227c81f7a3025f1
Clarence289 <sitholeclarence9@gmail>
2023-11-11 08:59:44 +0200

    my commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import GeneratePayment from './components/screens/GeneratePayment';
 // import Verification from './components/Screens/Verification';
 import TransactionHistory from './components/screens/TransactionHistory';
+import React from "react";  
 
 function App() {
   const [content, setContent] = React.useState("Home Page");
 
   return (
     <div className="App">
-      <TransactionHistory />
+      <GeneratePayment />
+      {/* <TransactionHistory /> */}
       {/* <Verification />	 */}
       
     </div>
